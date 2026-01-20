d1e2fd957ec08b8db3f65c9773c4705b6ea879c1
Soham Sharma <sohamkumarsharma@gmail.com>
2026-01-20 15:30:11 -0500

    add basic tfidf extraction func

--- src/features.py
+++ src/features.py
@@ -1,4 +1,6 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 def get_basic_tfidf(corpus):
-    pass
+    vectorizer = TfidfVectorizer(max_features=5000)
+    features = vectorizer.fit_transform(corpus)
+    return features, vectorizer
