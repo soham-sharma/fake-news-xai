6f3e8a51ed461346f19c82badfaa9db5401b2e50
Soham Sharma <sohamkumarsharma@gmail.com>
2026-02-07 11:05:00 -0500

    delete dataset dummy test

--- src/approach_b/dataset_dummy.py
+++
@@ -1,6 +0,0 @@
-# Playing around with huggingface dataset formatting
-from datasets import Dataset
-import pandas as pd
-df = pd.DataFrame({"text": ["hello", "world"], "label": [0, 1]})
-dataset = Dataset.from_pandas(df)
-print(dataset[0])
