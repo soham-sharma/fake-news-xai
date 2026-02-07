f391cc82b4b911cd9137235eaed5ce6a01004be0
Soham Sharma <sohamkumarsharma@gmail.com>
2026-02-07 12:35:10 -0500

    add tokenization wrapper for batched dataset inputs

--- src/approach_b/train.py
+++ src/approach_b/train.py
@@ -10,3 +10,14 @@ class BERTClassifier:
     def train(self):
         # TODO: training loop
         pass
+
+    def tokenize(self, texts, max_length=256):
+        import pandas as pd
+        from datasets import Dataset
+        text_list = pd.Series(texts).astype(str).tolist()
+        dataset = Dataset.from_dict({"text": text_list})
+        
+        def _tokenize(batch):
+            return self.tokenizer(batch["text"], padding=True, truncation=True, max_length=max_length)
+            
+        return dataset.map(_tokenize, batched=True).remove_columns(["text"])
