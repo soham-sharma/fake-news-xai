260a860484f8d8e32d06b0e5e0bd782d58a930e8
Soham Sharma <sohamkumarsharma@gmail.com>
2026-02-13 12:05:30 -0500

    wip wrapper for fast shap inference

--- src/approach_b/explain.py
+++ src/approach_b/explain.py
@@ -6,3 +6,7 @@ class BERTShapExplainer:
         self.model = model
         self.tokenizer = tokenizer
         self.masker = shap.maskers.Text(tokenizer)
+    
+    def _predict_proba_wrapper(self, texts):
+        # need to bypass huggingface dataset overhead for shap permutations
+        pass
