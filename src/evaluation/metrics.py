e9a84dfe95d08121b897beef1a101e5ed76cb8da
Soham Sharma <sohamkumarsharma@gmail.com>
2026-02-21 10:15:30 -0500

    init metrics logic to compare dt and bert models directly

---
+++ src/evaluation/metrics.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+
+class ComparisonMetrics:
+    def __init__(self):
+        self.results_dir = "results/"
+        
+    def accuracy_comparison(self, dt_metrics, bert_metrics):
+        # TODO: table comparing basic accuracy
+        pass
