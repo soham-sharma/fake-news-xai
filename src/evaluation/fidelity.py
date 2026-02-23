20629cfb2d21a4a09f785d6b7ff837bfa691d516
Soham Sharma <sohamkumarsharma@gmail.com>
2026-02-23 09:12:44 -0500

    start fidelity measurement logic. wait i have a typo

---
+++ src/evaluation/fidelity.py
@@ -0,0 +1,8 @@
+class FidelityEvaluator:
+    def __init__(self, model_predict_fn, feature_type):
+        self.model_predict_fn = model_predict_fn
+        self.feature_type = feature_type
+        
+    def sufficiency_score( self, X_samples, shap_values, top_k=5):
+        # wait why is my python throwing error with this argument word 
+        pass
