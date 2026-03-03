2fbf016cb0f8cf59f6ef5d842f5003c4a03b970f
Soham Sharma <sohamkumarsharma@gmail.com>
2026-03-03 10:05:00 -0500

    init run_experiments orchestrator pipeline

---
+++ src/run_experiments.py
@@ -0,0 +1,16 @@
+import argparse
+import pandas as pd
+
+class ExperimentRunner:
+    def __init__(self, config_path):
+        pass
+        
+    def run_approach_a(self):
+        print("Running DT models")
+        
+    def run_approach_b(self):
+        print("Running BERT models")
+
+if __name__ == "__main__":
+    runner = ExperimentRunner("config.yaml")
+    runner.run_approach_a()
