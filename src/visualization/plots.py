99811c7edc0cf368081f0695a5607c6dd49d1da7
Soham Sharma <sohamkumarsharma@gmail.com>
2026-03-11 09:12:00 -0400

    init results visualizer for plotting experiment data

---
+++ src/visualization/plots.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+
+class ResultsVisualizer:
+    def __init__(self):
+        pass
+        
+    def plot_accuracy_tradeoff(self):
+        print("plotting tradeoff curve")
